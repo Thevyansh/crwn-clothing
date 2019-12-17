b0a04213c6e66dd8908d5fd2a369f510b5314e38
Thevyansh <thevyansh@gmail.com>
2019-12-17 11:30:57 +0530

    Added styled-components to all components plus pages

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {createStructuredSelector} from "reselect";
-import './collection-overview.styles.scss'
-import {selectCollectionForPreview} from "../../redux/shop/shop.selectors";
-import CollectionPreview from "../collection-preview/collection-preview.component";
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
-const CollectionOverview = ({collections}) => {
-    return (
-        <div className='collection-overview'>
-            {collections.map(({id, ...otherCollectionProps}) => (
-                <CollectionPreview key={id} {...otherCollectionProps} />
-            ))}
-        </div>
-    );
-};
+import CollectionPreview from '../collection-preview/collection-preview.component';
+
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
+
+import { CollectionsOverviewContainer } from './collections-overview.styles';
+
+const CollectionsOverview = ({ collections }) => (
+  <CollectionsOverviewContainer>
+    {collections.map(({ id, ...otherCollectionProps }) => (
+      <CollectionPreview key={id} {...otherCollectionProps} />
+    ))}
+  </CollectionsOverviewContainer>
+);
+
 const mapStateToProps = createStructuredSelector({
-    collections: selectCollectionForPreview
+  collections: selectCollectionsForPreview
 });
-export default connect(mapStateToProps)(CollectionOverview);
+
+export default connect(mapStateToProps)(CollectionsOverview);
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,48 +1,49 @@
-import firebase from "firebase/app";
-import 'firebase/firestore'
-import 'firebase/auth'
-
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
 
 const config = {
-    apiKey: "AIzaSyCTPokq1yicV2aDLUtsfLYvheXs3624Jrw",
-    authDomain: "cwrn-db-c575d.firebaseapp.com",
-    databaseURL: "https://cwrn-db-c575d.firebaseio.com",
-    projectId: "cwrn-db-c575d",
-    storageBucket: "cwrn-db-c575d.appspot.com",
-    messagingSenderId: "343769525761",
-    appId: "1:343769525761:web:1e8a717f58e1f77b8a5a00",
-    measurementId: "G-1DBEM4NC5D"
-};
-
-export const createUserProfileDocument = async (userAuth, additionalData) => {
-    if (!userAuth) return;
-    const userRef = firestore.doc(`users/${userAuth.uid}`);
-    const snapShot = await userRef.get();
-
-    if (!snapShot.exists) {
-        const {displayName, email} = userAuth;
-        const createdAt = new Date();
-        try {
-            await userRef.set({
-                displayName,
-                email,
-                createdAt,
-                ...additionalData
-            })
-        } catch (error) {
-            console.log('error creating user', error.message);
-        }
-    }
-    return userRef;
+  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
+  authDomain: 'crwn-db.firebaseapp.com',
+  databaseURL: 'https://crwn-db.firebaseio.com',
+  projectId: 'crwn-db',
+  storageBucket: 'crwn-db.appspot.com',
+  messagingSenderId: '850995411664',
+  appId: '1:850995411664:web:7ddc01d597846f65'
 };
 
 firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({prompt: 'select_account'});
+provider.setCustomParameters({ prompt: 'select_account' });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
